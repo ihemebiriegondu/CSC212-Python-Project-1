4262da5a71702d1ffe37f5deb0ca45b2d6f53a2b
ihemebiriegondu <ihemebiriegondu@gmail.com>
2022-11-19 15:58:36 +0100

    added updating information to created files feature

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 import tkinter.messagebox
+from tkinter import filedialog
+import json
 
 
 class MainPage:
@@ -8,13 +10,26 @@ class MainPage:
         #created the empty list to add all informations
         self.matrix = []
 
+        self.command = ''
+
         window = Tk()
         window.title("Python Project")
         window.geometry("500x500")
 
-        # set first frame with subject title
+        #set first frame
+        self.frame0 = Frame(window)
+        self.frame0.pack()
+
+        openfile_button = Button(self.frame0, text="Open existing file", font=(
+            'Arial 13'), command=self.openFile)
+        openfile_button.pack()
+
+        createfile_button = Button(self.frame0, text="create new file", font=(
+            'Arial 13'), command=self.createFile)
+        createfile_button.pack()
+
+        # set create new file frame with subject title
         self.frame1 = Frame(window)
-        self.frame1.pack()
 
         subject = Label(self.frame1, text="Enter the subject / course")
         subject.pack()
@@ -31,6 +46,10 @@ class MainPage:
         submit_button = Button(self.frame1, text="Submit", font=(
             'Arial 13'), command=self.showFields)
         submit_button.pack()
+
+        back_button = Button(self.frame1, text="Back", font=(
+            'Arial 13'), command=self.backToFrame0Btn)
+        back_button.pack()
 
         # second frame with title
         self.frame2 = Frame(window)
@@ -82,6 +101,27 @@ class MainPage:
 
         window.mainloop()
 
+    def openFile(self):
+        self.filename = filedialog.askopenfilename(initialdir='/', title='Open file', filetypes = (("Json file","*.json"),("xml files","*.xml")))
+        if self.filename != '':
+            self.frame2.pack()
+            self.frame0.pack_forget()
+
+            self.command = 'open file'
+
+    def createFile(self):
+        self.filename = filedialog.asksaveasfilename(initialdir='/', title='Save file', defaultextension=".json")
+
+        if self.filename != '':
+            self.frame1.pack()
+            self.frame0.pack_forget()
+
+            self.command = 'create file'
+
+    def backToFrame0Btn(self):
+        self.frame0.pack()
+        self.frame1.pack_forget()
+
     def showFields(self):
         courseTitleInput = self.courseInput.get()
 
@@ -95,8 +135,10 @@ class MainPage:
             self.courseInputValue['text'] = self.courseInput.get()
 
             #created the file for adding all infos
-            self.newfile = open('newfile.csv', "w")
-            self.newfile.writelines(self.matrix)
+            
+            #self.newfile = open('newfile.json', "w", encoding='UTF8', newline='')
+            #self.newfile.writelines(self.matrix)
+            #self.newfile.close()
         else:
             tkinter.messagebox.showinfo("Input", "Subject field is empty!")
 
@@ -110,8 +152,22 @@ class MainPage:
         #appending the new row to the main list
         self.matrix.append(current_row)
 
-        self.newfile.writelines(self.matrix)
-        self.newfile.close()
+        if self.command == 'create file':
+            with open(self.filename, 'w') as outfile:
+                json.dump(self.matrix, outfile)
+        elif self.command == 'open file':
+            with open(self.filename, 'r') as openfile:
+                json_object = json.load(openfile)
+                json_object.append(current_row)
+
+                #print(json_object)
+
+            with open(self.filename, 'w') as outfile:
+                json.dump(json_object, outfile)
+
+        #self.newfile = open('newfile.csv', "a")
+        #self.newfile.writelines(self.matrix)
+        #self.newfile.close()
 
         #clearing the input fields after submission for new entry
         self.MatricNo.delete(0, END)
@@ -126,7 +182,7 @@ class MainPage:
         #autofocusing the first input field
         self.MatricNo.focus()
 
-        print(self.matrix)
+        #print(self.matrix)
 
 
     def clearEntry(self):
